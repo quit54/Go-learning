281c668be9296f0df8b43ad77ea892c19d664907
niliuhe <2783743448@qq.com>
2025-11-08 10:17:53 +0800

    协程之间相互监督

--- channel/test_3.go
+++ channel/test_3.go
@@ -9,14 +9,19 @@ func main() {
 		for i := 1; i < 6; i++ {
 			c <- i
 		}
-		close(c)
+		// close(c)
+		//这里close（）语句的进行与否，主要查看c的传递数值仍然存在，如果不存在就会结束
+		//如果不存在close（）这样的语句，会有死锁发生
 	}()
-	for {
-		if a, ok := <-c; ok {
-			fmt.Println(a)
-		} else {
-			break
-		}
+	// for {
+	// 	if a, ok := <-c; ok {
+	// 		fmt.Println(a)
+	// 	} else {
+	// 		break
+	// 	}
+	// }
+	for data := range c {
+		fmt.Println(data)
 	}
 	fmt.Println("主进程结束。")
 }
