1cd1debed03ae596e8a9aa50dda1d7760f47c497
niliuhe <2783743448@qq.com>
2025-12-24 17:23:09 +0800

    实现两个功能：1.用户在线指令查询2.用户超时强踢功能

--- golong-IM-systerm/server.go
+++ golong-IM-systerm/server.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"net"
 	"sync"
+	"time"
 )
 
 type Server struct {
@@ -45,7 +46,7 @@ func (this *Server) ListenMessage() {
 // 广播消息的方法，广播一个什么样子的消息
 // 这里的广播消息，是在已经上线的用户（不包含新上线用户）中，还是在新上线的用户中，还是所有在线用户？
 func (this *Server) BroadCast(user *User, msg string) {
-	Sendmsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+	Sendmsg := msg
 
 	this.Message <- Sendmsg
 
@@ -56,6 +57,9 @@ func (this *Server) Handler(conn net.Conn) {
 	//当前用户上线了，将用户加入OnlineMap中，进行广播
 	user := NewUser(conn, this)
 	user.Online()
+	//监听用户是否活跃
+	isLive := make(chan bool)
+
 	go func() {
 		buf := make([]byte, 4096)
 		for {
@@ -69,14 +73,33 @@ func (this *Server) Handler(conn net.Conn) {
 				return
 			}
 			//提取用户消息，去除"\n"，
-			msg := string(buf[:n-2])
+			msg := string(buf[:n-1])
 
 			//将得到的消息广播
 			user.DoMessage(msg)
+
+			//表示用户当前活跃，任意动作可行
+			isLive <- true
 		}
 	}()
 	//当前hander堵塞
-	select {}
+	for { //超时强踢功能开发
+		select {
+		case <-isLive:
+			//当前用户活跃，应重置定时器
+			//不用做任何事情，为了激活select
+
+		case <-time.After(time.Second * 20):
+			//已经超时，将会强制关闭user
+			user.Sendmsg("你被踢出")
+			//销毁用户资源
+			close(user.C)
+			//收回管道
+			conn.Close()
+			//退出当前Hander
+			return
+		}
+	}
 }
 func (this *Server) Start() {
 	//socket listen
--- golong-IM-systerm/user.go
+++ golong-IM-systerm/user.go
@@ -1,6 +1,10 @@
 package main
 
-import "net"
+import (
+	"fmt"
+	"net"
+	"strings"
+)
 
 type User struct {
 	Name string
@@ -11,7 +15,7 @@ type User struct {
 	server *Server
 }
 
-//创建一个用户的API
+// 创建一个用户的API
 func NewUser(conn net.Conn, server *Server) *User {
 	userAddr := conn.RemoteAddr().String()
 	user := &User{
@@ -27,7 +31,7 @@ func NewUser(conn net.Conn, server *Server) *User {
 	return user
 }
 
-//上线功能
+// 上线功能
 func (this *User) Online() {
 
 	this.server.mapLock.Lock()
@@ -36,17 +40,19 @@ func (this *User) Online() {
 	this.server.mapLock.Unlock()
 
 	//广播当前用户的上线消息
-	this.server.BroadCast(this, "已上线")
+	upmsg := fmt.Sprintf("[%s]%s:已上线\n", this.Addr, this.Name)
+	this.server.BroadCast(this, upmsg)
 }
 
-//下线功能
+// 下线功能
 func (this *User) Offline() {
 	this.server.mapLock.Lock()
 	delete(this.server.OnlineMap, this.Name)
 	this.server.mapLock.Unlock()
 
 	//广播当前用户的上线消息
-	this.server.BroadCast(this, "下线")
+	downmsg := fmt.Sprintf("[%s]%s:已下线\n", this.Addr, this.Name)
+	this.server.BroadCast(this, downmsg)
 }
 
 func (this *User) Sendmsg(msg string) {
@@ -59,16 +65,34 @@ func (this *User) DoMessage(msg string) {
 
 		this.server.mapLock.Lock()
 		for _, user := range this.server.OnlineMap {
-			onlinemsg := "[" + user.Addr + "]" + user.Name + "在线...."
+			onlinemsg := fmt.Sprintf("[%s][%s]在线....\n", user.Addr, user.Name)
 			this.Sendmsg(onlinemsg)
 		}
 		this.server.mapLock.Unlock()
 
+	} else if len(msg) > 7 && msg[:7] == "rename|" {
+		//消息格式：“rename|张三”
+		newName := strings.Split(msg, "|")[1]
+
+		//判断name是否存在
+		_, ok := this.server.OnlineMap[newName]
+		if ok {
+			this.Sendmsg("当前用户名被使用\n")
+		} else {
+			this.server.mapLock.Lock()
+			delete(this.server.OnlineMap, this.Name)
+			this.server.OnlineMap[newName] = this
+			this.server.mapLock.Unlock()
+
+			this.Name = newName
+			this.Sendmsg("您已经更新用户名：" + this.Name + "\n")
+		}
+	} else {
+		this.server.BroadCast(this, msg)
 	}
-	this.server.BroadCast(this, msg)
 }
 
-//监听当前User channel的方法，一旦有消息，就直接发送给对端客户端。
+// 监听当前User channel的方法，一旦有消息，就直接发送给对端客户端。
 func (this *User) ListenMessage() {
 	for {
 		msg := <-this.C
