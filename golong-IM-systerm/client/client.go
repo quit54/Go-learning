2a6a3f70780ab9779463df59d9c6833036018619
niliuhe <2783743448@qq.com>
2025-12-27 16:21:01 +0800

    新增客户端功能,并且实现菜单选项

--- golong-IM-systerm/client/client.go
+++ golong-IM-systerm/client/client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 )
@@ -10,12 +11,14 @@ type Client struct {
 	ServerPort int
 	Name       string
 	conn       net.Conn
+	flag       int
 }
 
 func NewClient(serverip string, serverport int) *Client {
 	client := &Client{
 		ServerIp:   serverip,
 		ServerPort: serverport,
+		flag:       999,
 	}
 	Conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", serverip, serverport))
 	if err != nil {
@@ -26,8 +29,53 @@ func NewClient(serverip string, serverport int) *Client {
 
 	return client
 }
+func (client *Client) menu() bool {
+	var flag int
+	fmt.Println("1.公聊模式")
+	fmt.Println("2.私聊模式")
+	fmt.Println("3.更新用户列表")
+	fmt.Println("0.退出系统")
+
+	fmt.Scanln(&flag)
+
+	if flag >= 0 && flag <= 3 {
+		client.flag = flag
+		return true
+	} else {
+		fmt.Println(">>>>请输入合法数字")
+		return false
+	} //检查flag输入是否合法
+}
+
+var serverIp string
+var serverPort int
+
+func init() {
+	//命令行解析，调用库函数
+	flag.StringVar(&serverIp, "ip", "127.0.0.1", "设置服务器IP地址:(默认是127.0.0.1)")
+	flag.IntVar(&serverPort, "port", 8888, "设置服务器端口(默认是8888")
+}
+func (client *Client) Run() {
+	for client.flag != 0 {
+		for client.menu() != true {
+		}
+		switch client.flag {
+		case 1:
+			fmt.Println("公聊模式选择--------")
+			break
+		case 2:
+			fmt.Println("私聊模式选择---------")
+			break
+		case 3:
+			fmt.Println("更新用户列表选择--------")
+			break
+		}
+	}
+}
 func main() {
-	client := NewClient("127.0.0.1", 8888)
+	flag.Parse()
+
+	client := NewClient(serverIp, serverPort)
 	if client == nil {
 		fmt.Println(">>>>>>客户端连接失败。。。")
 		return
@@ -35,9 +83,5 @@ func main() {
 	fmt.Println(">>>>>>连接服务器成功。。。")
 
 	//保持客户端运行逻辑
-	for {
-		select {
-		default:
-		}
-	}
+	client.Run()
 }
